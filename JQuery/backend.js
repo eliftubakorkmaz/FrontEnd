db55a2128b6cbea8b0ebdbb925803e9243e8bc7f
Elif Tuba Korkmaz <etubasavli@gmail.com>
2023-12-08 10:52:36 +0300

    todo api jquery

---
+++ JQuery/backend.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const todos = [];
+
+app.get("", (req,res) => {
+    res.json({message: "Api çalışıyor"});
+});
+
+app.get("/api/getAll", (req,res) => {
+    res.json(todos);
+});
+
+app.post("/api/create", (req, res) => {
+    const body = req.body;
+    todos.push(body);
+    res.json();
+});
+
+app.listen(3200, () => console.log("localhost:3200 üzerinden ayakta"));
