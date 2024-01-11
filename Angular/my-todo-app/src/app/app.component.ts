b1d530da95f5979f3ad3cdc7dd6b76ee46ed6aa2
etubas <etubasavli@gmail.com>
2024-01-11 16:43:12 +0300

    todoapp by angular

--- Angular/my-todo-app/src/app/app.component.ts
+++ Angular/my-todo-app/src/app/app.component.ts
@@ -2,9 +2,60 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  template: `
+  <h1>My Todo App</h1>
+  <!-- Create Elements -->
+  <div *ngIf="!isUpdateFormActive">
+    <input #inputEl type="text" [(ngModel)]="work">
+    <button (click)="save(inputEl)">Save</button>
+  </div>
+  <!-- Create Elements -->
+  <br>
+  <!-- Update Elements -->
+  <div *ngIf="isUpdateFormActive">
+    <input type="text" [(ngModel)]="updateWork">
+    <button (click)="update()">Update</button>
+    <button (click)="cancel()">Cancel</button>
+  </div>
+  <!-- Update Elements -->
+  <hr>
+  <ul>
+    <li *ngFor="let t of todos; let i = index"> 
+      {{ t }}
+      <button *ngIf="!isUpdateFormActive" (click)="get(i)">Update</button>
+      <button *ngIf="!isUpdateFormActive" (click)="removeByIndex(i)">Remove</button>
+    </li>
+  </ul>`
 })
 export class AppComponent {
-  title = 'my-todo-app';
+  work: string = "";
+  updateWork: string = "";
+  updateIndex: number = 0;
+  isUpdateFormActive: boolean = false;
+  todos: string[] = [];
+
+  save(inputEl: HTMLInputElement){
+    this.todos.push(this.work);
+    this.work = "";
+    inputEl.focus();
+  }
+
+  removeByIndex(index: number){
+    this.todos.splice(index,1);
+  }
+
+  get(index: number){
+    this.updateWork = this.todos[index];
+    this.updateIndex = index;
+    this.isUpdateFormActive = true;
+  }
+
+  update(){
+    this.todos[this.updateIndex] = this.updateWork;
+    this.isUpdateFormActive = false;
+  }
+
+  cancel(){
+    this.isUpdateFormActive = false;
+  }
 }
